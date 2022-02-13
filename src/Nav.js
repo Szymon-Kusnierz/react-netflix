b26f64859c570f9b98ff332a5c7a99e082e6f9e3
Szymon Kusnierz <kusnierzs@hotmail.com>
2022-02-13 19:30:12 +0000

    init

---
+++ src/Nav.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import './Nav.css';
+
+function Nav() {
+const [show, handleShow] = useState(false);
+
+const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+        handleShow(true);
+    } else {
+        handleShow(false);
+    }
+}
+
+useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+}, [])
+
+  return (
+    <div className={`nav ${show && 'nav_black'}`}>
+        <div className="nav_contents">
+
+        </div>
+        <img 
+        className="nav_logo"
+        src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" 
+        alt=""
+        />
+
+        <img 
+        className="nav_avatar"
+        src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png" 
+        alt=""
+        />
+    </div>
+  );
+}
+
+export default Nav;
